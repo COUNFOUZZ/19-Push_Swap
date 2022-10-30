43409ec07b99d365bfe2bf86fb7c223fbf5f98ef
Alexis Abda <alexis.abda@hotmail.com>
2022-10-30 20:46:45 +0100

    Add ft_rotate

--- include/push_swap.h
+++ include/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: aabda <aabda@student.s19.be>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/14 23:04:13 by aabda             #+#    #+#             */
-/*   Updated: 2022/10/29 17:49:49 by aabda            ###   ########.fr       */
+/*   Updated: 2022/10/30 20:41:45 by aabda            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,7 @@
 # include <unistd.h>
 # include <stdlib.h>
 # include <limits.h>
-#include <stdio.h>
-
+# include <stdio.h>	//	A DELETE
 
 # define ERR_ARG "Invalide number of argument !\n"
 # define ERROR "Error\n"
@@ -28,16 +27,29 @@ typedef struct s_data
 	struct s_data	*next;
 }	t_data;
 
-t_data	*ft_add_node(t_data *node, int val);
+/*		LIBFT		*/
 size_t	ft_strlen(const char *s);
-char	**ft_split(char const *s, char c);
 int		ft_atoi(const char *str, t_data *node);
-int		ft_is_double(t_data *node);
+char	**ft_split(char const *s, char c);
 void	ft_putstr_fd(const char *s, int fd);
-void	ft_error(t_data *node, int res, const char *error);
+
+/*		NODE.C		*/
+t_data	*ft_add_node(t_data *node, int val);
 void	ft_print_list(t_data *node);
-void	ft_if_is_two_args(int argc, const char **argv, t_data *stack);
+int		ft_is_double(t_data *node);
+
+/*		FT_SWAP.C		*/
 void	ft_swap(const char *s, t_data **stack);
 void	do_ss(t_data **stack_a, t_data **stack_b);
+
+/*		FT_ROTATE.C		*/
+void	ft_rotate(const char *s, t_data **stack);
+void	do_rr(t_data **stack_a, t_data **stack_b);
+
+/*		LOGIC.C		*/
+void	ft_if_is_two_args(int argc, const char **argv, t_data *stack);
+
+/*		UTILS.C		*/
+void	ft_error(t_data *node, int res, const char *error);
 
 #endif
---
+++ srcs/ft_rotate.c
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_rotate.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aabda <aabda@student.s19.be>               +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/30 20:33:16 by aabda             #+#    #+#             */
+/*   Updated: 2022/10/30 20:33:42 by aabda            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../include/push_swap.h"
+
+void	ft_rotate(const char *s, t_data **stack)
+{
+	t_data	*first;
+	t_data	*second;
+	t_data	*last;
+
+	last = NULL;
+	if (stack && *stack)
+	{
+		first = *stack;
+		second = first->next;
+		while ((*stack)->next)
+			*stack = (*stack)->next;
+		last = *stack;
+		last->next = first;
+		*stack = second;
+		first->next = NULL;
+		ft_putstr_fd(s, 1);
+	}
+}
+
+void	do_rr(t_data **stack_a, t_data **stack_b)
+{
+	ft_rotate(NULL, &*stack_a);
+	ft_rotate(NULL, &*stack_b);
+	ft_putstr_fd("[RR]\n", 1);
+}
