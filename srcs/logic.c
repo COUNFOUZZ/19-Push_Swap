7b8458455b517377fc9d690a3c88ff4c28156d25
Alexis Abda <alexis.abda@hotmail.com>
2022-11-02 14:41:18 +0100

    Delete print function

--- srcs/logic.c
+++ srcs/logic.c
@@ -6,7 +6,7 @@
 /*   By: aabda <aabda@student.s19.be>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/24 18:50:32 by aabda             #+#    #+#             */
-/*   Updated: 2022/10/24 18:58:11 by aabda            ###   ########.fr       */
+/*   Updated: 2022/11/02 14:40:44 by aabda            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,6 +27,5 @@ void	ft_if_is_two_args(int argc, const char **argv, t_data *stack)
 		while (tab[--i])
 			stack = ft_add_node(stack, ft_atoi(tab[i], stack));
 		ft_error(stack, ft_is_double(stack), ERROR);
-		ft_print_list(stack);
 	}
 }
