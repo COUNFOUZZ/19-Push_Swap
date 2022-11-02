0df316e69e4e73a1f1bdd65db768de216d70a042
Alexis Abda <alexis.abda@hotmail.com>
2022-11-02 10:51:40 +0100

    Delete unused variable

--- srcs/ft_push.c
+++ srcs/ft_push.c
@@ -6,7 +6,7 @@
 /*   By: aabda <aabda@student.s19.be>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/30 23:10:54 by aabda             #+#    #+#             */
-/*   Updated: 2022/10/31 11:54:21 by aabda            ###   ########.fr       */
+/*   Updated: 2022/11/02 10:43:04 by aabda            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,10 @@
 
 void	pa(t_data **stack_a, t_data **stack_b)
 {
-	t_data	*first_a;
 	t_data	*first_b;
 
 	if (!stack_b)
 		return ;
-	first_a = *stack_a;
 	first_b = *stack_b;
 	*stack_b = (*stack_b)->next;
 	first_b->next = *stack_a;
@@ -30,12 +28,10 @@ void	pa(t_data **stack_a, t_data **stack_b)
 void	pb(t_data **stack_a, t_data **stack_b)
 {
 	t_data	*first_a;
-	t_data	*first_b;
 
 	if (!stack_a)
 		return ;
 	first_a = *stack_a;
-	first_b = *stack_b;
 	*stack_a = (*stack_a)->next;
 	first_a->next = *stack_b;
 	*stack_b = first_a;
