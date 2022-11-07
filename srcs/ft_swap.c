019f1e0b8a60d01b9ce7ec959021885d1e683013
Alexis Abda <alexis.abda@hotmail.com>
2022-11-07 14:47:34 +0100

    fix useless syntax

--- srcs/ft_swap.c
+++ srcs/ft_swap.c
@@ -6,7 +6,7 @@
 /*   By: aabda <aabda@student.s19.be>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/29 16:16:35 by aabda             #+#    #+#             */
-/*   Updated: 2022/10/30 23:17:00 by aabda            ###   ########.fr       */
+/*   Updated: 2022/11/04 16:08:57 by aabda            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,7 +35,7 @@ void	ft_swap(const char *s, t_data **stack)
 
 void	ss(t_data **stack_a, t_data **stack_b)
 {
-	ft_swap(NULL, &*stack_a);
-	ft_swap(NULL, &*stack_b);
+	ft_swap(NULL, stack_a);
+	ft_swap(NULL, stack_b);
 	ft_putstr_fd("[SS]\n", 1);
 }
