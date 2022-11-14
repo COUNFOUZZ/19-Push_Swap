5911e0ec2f724633a49088e93cbc944cd1cf9cec
Alexis Abda <alexis.abda@hotmail.com>
2022-11-14 16:34:45 +0100

    update main

--- srcs/main.c
+++ srcs/main.c
@@ -6,37 +6,11 @@
 /*   By: aabda <aabda@student.s19.be>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/14 23:03:56 by aabda             #+#    #+#             */
-/*   Updated: 2022/11/02 10:45:03 by aabda            ###   ########.fr       */
+/*   Updated: 2022/11/14 15:56:27 by aabda            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/push_swap.h"
-
-// int	main(int argc, const char **argv)
-// {
-// 	t_data	*stack_a;
-// 	char	**tab;
-
-// 	stack_a = NULL;
-// 	if (argc < 2)
-// 		return (-1);
-// 	else if (argc == 2)
-// 		ft_if_is_two_args(argc, argv, stack_a);
-// 	else
-// 	{
-// 		while (--argc >= 1)
-// 			stack_a = ft_add_node(stack_a, ft_atoi(argv[argc], stack_a));
-// 		ft_error(stack_a, ft_is_double(stack_a), ERROR);
-// 		ft_print_list(stack_a);
-// 		ft_swap("[SA]\n", &stack_a);
-// 		ft_print_list(stack_a);
-// 		ft_rotate("[RA]\n", &stack_a);
-// 		ft_print_list(stack_a);
-// 		ft_reverse_rotate("[RRA]\n", &stack_a);
-// 		ft_print_list(stack_a);
-// 	}
-// 	return (0);
-// }
 
 int	main(int argc, const char **argv)
 {
@@ -46,19 +20,18 @@ int	main(int argc, const char **argv)
 	stack_a = NULL;
 	stack_b = NULL;
 	if (argc < 2)
-		return (-1);
+		ft_error(stack_a, stack_b, -1, ERROR);
 	else if (argc == 2)
-		ft_if_is_two_args(argc, argv, stack_a);
+	{
+		ft_if_is_two_args(argc, argv, &stack_a);
+		ft_sort(&stack_a, &stack_b);
+	}
 	else
 	{
 		while (--argc >= 1)
 			stack_a = ft_add_node(stack_a, ft_atoi(argv[argc], stack_a));
-		ft_error(stack_a, ft_is_double(stack_a), ERROR);
-		ft_print_two_stack(stack_a, stack_b);
-		pb(&stack_a, &stack_b);
-		ft_print_two_stack(stack_a, stack_b);
-		pa(&stack_a, &stack_b);
-		ft_print_two_stack(stack_a, stack_b);
+		ft_sort(&stack_a, &stack_b);
+		ft_error(stack_a, stack_b, ft_is_double(stack_a), ERROR);
 	}
 	return (0);
 }
