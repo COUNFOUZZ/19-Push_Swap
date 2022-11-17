ea4fd7e8c812ec0ac486fdf097ca5b576b6db9d8
Alexis Abda <alexis.abda@hotmail.com>
2022-11-17 15:34:12 +0100

    update main.c "Need to be at the norm"

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: aabda <aabda@student.s19.be>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/14 23:03:56 by aabda             #+#    #+#             */
-/*   Updated: 2022/11/16 20:46:11 by aabda            ###   ########.fr       */
+/*   Updated: 2022/11/17 15:24:40 by aabda            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,25 +23,26 @@ int	main(int argc, const char **argv)
 		ft_error(stack_a, stack_b, -1, ERROR);
 	else if (argc == 2)
 	{
-		// if (ft_is_sorted(stack_a) == 0)
-		// {
-		// 	ft_free_stack(stack_a, NULL);
-		// 	return (0);
-		// }
+		if (ft_is_sorted(stack_a) == 0)
+		{
+			ft_free_stack(stack_a, NULL);
+			return (0);
+		}
 		ft_if_is_two_args(argc, argv, &stack_a);
 		ft_sort(&stack_a, &stack_b);
 	}
 	else
 	{
-		// if (ft_is_sorted(stack_a) == 0)
-		// {
-		// 	ft_free_stack(stack_a, NULL);
-		// 	return (0);
-		// }
+		if (ft_is_sorted(stack_a) == 0)
+		{
+			ft_free_stack(stack_a, NULL);
+			return (0);
+		}
 		while (--argc >= 1)
 			stack_a = ft_add_node(stack_a, ft_atoi(argv[argc], stack_a));
-		ft_sort(&stack_a, &stack_b);
 		ft_error(stack_a, stack_b, ft_is_double(stack_a), ERROR);
+		ft_sort(&stack_a, &stack_b);
 	}
+	ft_free_stack(stack_a, NULL);
 	return (0);
 }
