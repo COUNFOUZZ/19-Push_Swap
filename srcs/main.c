f8014214c3c8e8f2586e474aaf759ef88f2d20d2
Alexis Abda <alexis.abda@hotmail.com>
2022-11-17 10:54:39 +0100

    update main.c

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: aabda <aabda@student.s19.be>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/14 23:03:56 by aabda             #+#    #+#             */
-/*   Updated: 2022/11/14 15:56:27 by aabda            ###   ########.fr       */
+/*   Updated: 2022/11/16 20:46:11 by aabda            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,11 +23,21 @@ int	main(int argc, const char **argv)
 		ft_error(stack_a, stack_b, -1, ERROR);
 	else if (argc == 2)
 	{
+		// if (ft_is_sorted(stack_a) == 0)
+		// {
+		// 	ft_free_stack(stack_a, NULL);
+		// 	return (0);
+		// }
 		ft_if_is_two_args(argc, argv, &stack_a);
 		ft_sort(&stack_a, &stack_b);
 	}
 	else
 	{
+		// if (ft_is_sorted(stack_a) == 0)
+		// {
+		// 	ft_free_stack(stack_a, NULL);
+		// 	return (0);
+		// }
 		while (--argc >= 1)
 			stack_a = ft_add_node(stack_a, ft_atoi(argv[argc], stack_a));
 		ft_sort(&stack_a, &stack_b);
